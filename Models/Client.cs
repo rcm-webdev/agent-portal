86b2804959ddeb8b9c2dcbd5adef9973096950c2
aokiji <rcm.webdev.pro@gmail.com>
2025-10-15 16:19:31 -0700

    add validation error on form submission

--- Models/Client.cs
+++ Models/Client.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace agent_portal.Models;
 
 public class Client
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required")]
     public string? Name { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Please enter a valid email address")]
     public string? Email { get; set; }
+
     public string? Phone { get; set; }
     public string? Address { get; set; }
     public ICollection<Policy>? Policies { get; set; }
--- Models/InsuranceProduct.cs
+++ Models/InsuranceProduct.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace agent_portal.Models;
 
 public class InsuranceProduct
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required")]
     public string? Name { get; set; } // e.g., "Homeowners", "Auto", "Farm"
+
+    [Required(ErrorMessage = "Type is required")]
     public string? Type { get; set; } // Category
+
     public string? CoverageSummary { get; set; } // Brief description
     public string? EligibilityNotes { get; set; } // Who qualifies
     public string? QuickLinks { get; set; } // URLs or attachments
