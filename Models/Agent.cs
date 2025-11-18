8cd1373d3367ec1e208744cbac855cd8d61aeb16
aokiji <rcm.webdev.pro@gmail.com>
2025-11-17 20:04:00 -0800

    update header for Agents models

--- Models/Agent.cs
+++ Models/Agent.cs
@@ -7,12 +7,14 @@ public class Agent
     public int Id { get; set; }
 
     [Required(ErrorMessage = "Name is required")]
+    [Display(Name = "Agents Name")]
     public string? Name { get; set; }
 
     [Required(ErrorMessage = "Email is required")]
     [EmailAddress(ErrorMessage = "Please enter a valid email address")]
     public string? Email { get; set; }
 
+    [Display(Name = "Phone Number")]
     public string? Phone { get; set; }
     public string? Agency { get; set; }
     public ICollection<Policy>? Policies { get; set; }
